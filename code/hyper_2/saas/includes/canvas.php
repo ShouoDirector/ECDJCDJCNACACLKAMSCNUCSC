0490adb43545e08dcb8e87e61831f5d4144d0945
ShouoDirector <ivanmilesmvista@gmail,com>
2023-05-19 17:20:02 +0800

    Version 1.0.0.5

--- code/hyper_2/saas/includes/canvas.php
+++ code/hyper_2/saas/includes/canvas.php
@@ -822,10 +822,6 @@
             <div class="col-6">
                 <button type="button" class="btn btn-light w-100" id="reset-layout">Reset</button>
             </div>
-            <div class="col-6">
-                <a href="https://themes.getbootstrap.com/product/hyper-responsive-admin-dashboard-template/"
-                    target="_blank" role="button" class="btn btn-primary w-100">Buy Now</a>
-            </div>
         </div>
     </div>
 </div>
